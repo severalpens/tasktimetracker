249062b101aa470f83ad11b02d6e8b0d12d622e6
Severalpens <Paul.Collins@ibm.com>
2022-05-25 03:00:21 +1000

    add web.config and api.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
